1dcb559d54caeffe018dc93566b82ec002d902a9
Tạ Quang Khôi <khoi0941@gmail.com>
2022-07-31 21:02:01 +0700

    Thêm cmt và xoá dòng code gây việc ko debug được

--- app/src/main/java/com/example/napkin/MainActivity.java
+++ app/src/main/java/com/example/napkin/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.napkin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnSend;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
     }
 }
