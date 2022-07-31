0ffc8b550121f02aa6ea14704c3d864663699279
Tạ Quang Khôi <khoi0941@gmail.com>
2022-07-31 22:04:12 +0700

    - name EditText ed_thought

--- app/src/main/java/com/example/napkin/MainActivity.java
+++ app/src/main/java/com/example/napkin/MainActivity.java
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button btnSend;
+    EditText etThought;
+    TextView tvAppName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,10 +20,12 @@ public class MainActivity extends AppCompatActivity {
 
         btnSend = findViewById(R.id.btn_send);
 
+        etThought = findViewById(R.id.et_thought);
+
         Bundle bd = getIntent().getExtras();
-        if(bd != null){
+        if(bd != null) {
             String code = bd.getString("code");
-            Toast.makeText(this, code, Toast.LENGTH_SHORT).show();
+            etThought.setText(code);
         }
     }
 
