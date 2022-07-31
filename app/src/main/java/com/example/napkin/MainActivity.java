cf341e3d538be95a66dedddbbb3a861928e75a5a
Tạ Quang Khôi <khoi0941@gmail.com>
2022-07-31 21:28:48 +0700

    Add act GetCode

--- app/src/main/java/com/example/napkin/MainActivity.java
+++ app/src/main/java/com/example/napkin/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button btnSend;
@@ -12,6 +13,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        btnSend = findViewById(R.id.btn_send);
+
+        Bundle bd = getIntent().getExtras();
+        if(bd != null){
+            String code = bd.getString("code");
+            Toast.makeText(this, code, Toast.LENGTH_SHORT).show();
+        }
     }
+
+    private void SendThought() {
+
+    }
+
+
 }
