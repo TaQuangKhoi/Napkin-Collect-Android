10cd6084cba3fa4382817eb642f05d193ab0514b
Tạ Quang Khôi <khoi0941@gmail.com>
2022-08-01 22:25:13 +0700

    Tạo act chọn cách để đăng nhập

---
+++ app/src/main/java/com/example/napkin/ChooseLoginType.java
@@ -0,0 +1,14 @@
+package com.example.napkin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChooseLoginType extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_login_type);
+    }
+}
