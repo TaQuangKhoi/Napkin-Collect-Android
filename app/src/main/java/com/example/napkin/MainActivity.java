3c6b9495f445aa3359806913020197b3b99a9923
Tạ Quang Khôi <khoi0941@gmail.com>
2022-08-20 19:45:15 +0700

    import View class

--- app/src/main/java/com/example/napkin/MainActivity.java
+++ app/src/main/java/com/example/napkin/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 // import từ widget các View
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
