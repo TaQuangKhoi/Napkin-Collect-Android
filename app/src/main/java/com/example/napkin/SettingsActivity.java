6a83de018b2157a75362d39b1638f19005093f47
Tạ Quang Khôi <khoi0941@gmail.com>
2022-08-29 19:51:36 +0700

    Butotn save setting

--- app/src/main/java/com/example/napkin/SettingsActivity.java
+++ app/src/main/java/com/example/napkin/SettingsActivity.java
@@ -1,16 +1,41 @@
 package com.example.napkin;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
+    SharedPreferences savedSettings;
+    EditText etEmail, etToken;
+    Button btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
 
+        etEmail = findViewById(R.id.et_email);
+        etToken = findViewById(R.id.et_token);
+        btnSave = findViewById(R.id.btn_save);
+        savedSettings = getSharedPreferences("Settings", MODE_PRIVATE);
+
+        initButton();
+    }
+
+    private void initButton() {
+        btnSave.setOnClickListener(v -> {
+
+            savedSettings.edit().putString("email", etEmail.getText().toString()).apply();
+            savedSettings.edit().putString("token", etToken.getText().toString()).apply();
+            savedSettings.edit().commit();
+
+            Toast.makeText(this, "Saved Settings", Toast.LENGTH_SHORT).show();
+            finish();
+        });
     }
 
 }
