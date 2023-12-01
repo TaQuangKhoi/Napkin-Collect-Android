63d8d4f9f4469dc72903e0be8494e038717d774b
Tạ Quang Khôi <khoi0941@gmail.com>
2023-12-01 23:14:40 +0700

    Try @Preview for alone file

--- NapkinCollect/app/src/main/java/com/taquangkhoi/napkincollect/components/MainScreen.kt
+++ NapkinCollect/app/src/main/java/com/taquangkhoi/napkincollect/components/MainScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.taquangkhoi.napkincollect.R
 
@@ -140,4 +141,10 @@ fun MainScreen() {
 //            )
 //        }
 //    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun MainScreenPreview() {
+    MainScreen()
 }
